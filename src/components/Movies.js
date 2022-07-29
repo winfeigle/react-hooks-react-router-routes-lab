b3786945f3facd7867c905378bb8716ee94ca84e
Win Feigle <win.feigle@gmail.com>
2022-07-28 23:09:31 -0600

    Render movies to page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+    const renderMovies = movies.map(movie => {
+      
+      const genres = movie.genres.map(genre => {
+        return <li>{genre}</li>;
+      })
+
+      return(
+          <div>
+            <p><b>{movie.title}</b></p>
+            <p><em>{movie.time} minutes</em></p>
+            <ul>
+              { genres }
+            </ul>
+          </div>
+      );
+    })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    { renderMovies }
+  </div>
+  );
 }
 
 export default Movies;
